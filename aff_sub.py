2cbaabbb188f82cb532278d5101359dfbdf86717
omergabay2 <36948734+omergabay2@users.noreply.github.com>
2018-10-23 13:55:55 +0300

    recognize where they look

--- aff_sub.py
+++ aff_sub.py
@@ -29,11 +29,12 @@ def callback_affdex_data(msg):
     personality["Time"] = times
     personality["Joy"] = round(msg.emotions[0], 2)
     personality["x"] = x_now
+    personality["Look_at"] = ((x_now + 960) % 1920) - 2 * msg.measurements[2]*(1920/360)
 
     faces[face_index].append(personality)
 
-    new_data = "time:" + str(times) + "," + "Person ID:" + str(face_index+1) + "," + "Location:" + str(msg.face_points[26].x) + "," + "Joy:" + str(msg.emotions[0]) + "," + "Anger:" + str(msg.emotions[1]) + "," + "Didgust:" + str(msg.emotions[2]) + "," + "Contempt:" + str(msg.emotions[3]) + "," + "Engagement:" + str(msg.emotions[4]) + "," + "Fear:" + str(msg.emotions[5]) + "," + "Sadness:" + str(msg.emotions[6]) + "," + "Surprise:" + str(msg.emotions[7]) + "," + "Valence:" + str(msg.emotions[8])
-    print new_data
+    new_data = "time:" + str(times) + "," + "Person ID:" + str(face_index+1) + "," + "Location:" + str(msg.face_points[26].x) + "," + "LookAt:" + str(((x_now + 960) % 1920) - 2 * msg.measurements[2]*(1920/360)) + "," + "Joy:" + str(msg.emotions[0]) + "," + "Anger:" + str(msg.emotions[1]) + "," + "Didgust:" + str(msg.emotions[2]) + "," + "Contempt:" + str(msg.emotions[3]) + "," + "Engagement:" + str(msg.emotions[4]) + "," + "Fear:" + str(msg.emotions[5]) + "," + "Sadness:" + str(msg.emotions[6]) + "," + "Yaw:" + str(msg.measurements[1]) + "," + "Roll:" + str(msg.measurements[2]) + "," + "Pitch:" + str(msg.measurements[3]) + "," + "Surprise:" + str(msg.emotions[7]) + "," + "Valence:" + str(msg.emotions[8]) + "," + "Nose Tip" + str(msg.face_points[12].x) + "-" + str(msg.face_points[12].y) + "," + "Chin" + str(msg.face_points[2].x) + "-" + str(msg.face_points[2].y) + "," + "RightEyeOuter" + str(msg.face_points[16].x) + "-" + str(msg.face_points[16].y) + "," + "RightEyeInner" + str(msg.face_points[17].x) + "-" + str(msg.face_points[17].y) + "," + "LeftEyeInner" + str(msg.face_points[18].x) + "-" + str(msg.face_points[18].y) + "LeftEyeOuter" + str(msg.face_points[19].x) + "-" + str(msg.face_points[19].y) + "," + "RightLip Corener" + str(msg.face_points[20].x) + "-" + str(msg.face_points[20].y) + "," + "LeftLipCorner" + str(msg.face_points[24].x) + "-" + str(msg.face_points[24].y)
+
 
     if len(faces) > len(plots):
         plots = {}
@@ -46,10 +47,12 @@ def callback_affdex_data(msg):
         for personality in timeline:
             xs.append(personality['Time'])
             ys.append(personality['Joy'])
+            Look_At = personality['Look_at']
+
         this_plot = plots[index]
         this_plot.clear()
         this_plot.plot(xs, ys)
-        this_plot.set_title("person %d x: %d" % ((index + 1), (timeline[0]['x'])))
+        this_plot.set_title("person %d x: %d Look_at %d" % ((index + 1), (timeline[0]['x']), Look_At))
         this_plot.set_ylabel("Joy")
         this_plot.set_xlabel("Time(s)")
         plt.tight_layout()
